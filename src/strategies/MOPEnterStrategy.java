ce445712f6a4b7836e96eeb9f1a379f8462db410
NaczelnyRachmistrz <michkukula@gmail.com>
2018-02-28 23:17:50 +0100

    cleaning

--- src/strategies/MOPEnterStrategy.java
+++
@@ -1,8 +0,0 @@
-package strategies;
-/*
- * Interface used by agents. Extended classes implement decision function for entering a MOP.
- */
-public interface MOPEnterStrategy {
-	
-	public abstract boolean decide(float lastVisit, float travelTime, String vehicleType);
-}
