4bb0544d1468bae394d1743032c98cadaee8fe15
oroko123 <pperkowski96@gmail.com>
2018-03-15 21:47:17 +0100

    Added creators for Matsim xml input files

--- plancreator/FacilityPlanCreator.java
+++ plancreator/FacilityPlanCreator.java
@@ -1,25 +1,87 @@
-/* *********************************************************************** *
- * project: MOPSim
- * FacilityPlanCreator.java
- * written by: mopsy-team
- * ***********************************************************************/
 package plancreator;
 
+import java.io.*;
+
 public class FacilityPlanCreator {
-	
-	//Może trzeba trochę edytować Stringi z pythona
-    public static final String HEADER = "<?xml version=\"1.0\"?>\n<!DOCTYPE facilities SYSTEM \"http://www.matsim.org/files/dtd/facilities_v1.dtd\">\n";
-    public static final String FACILITY_HEAD = "<facilities name=\"mopy\" >\n";
-    public static final String FACILITY_FOOT = "<activity type=\"rest\">\n</activity>\n</facility>\n";
-    public static final String FOOTER = "</facilities>";
-    
-    
-    public static void createFacilityPlan(final String inputFilepath, final String outputFilepath ) {
-    	/*
-    	 * Tworzy plik xml matsima o ścieżce outputFilepath z plik csv o ścieżce inputFilepath. Plik xml_generator.py
-    	 * używał tylko csvki w której w i-tej linijce były tylko współrzędne MOP-a o id i, ale na pewno przydadzą sie też informacje o liczbie miejsc dla samochodów,
-    	 * ciężarówek i autobusów (co można dopisać w linijce dla odpowiedniego mopa np. carLimit=100 busLimit=19 truckLimit=10). Csvki można łatwo wygenerować z tego
-    	 * xmla co mamy.
-    	 */
+
+    private static final String HEADER = "<?xml version=\"1.0\"?>\n<!DOCTYPE facilities SYSTEM \"http://www.matsim.org/files/dtd/facilities_v1.dtd\">\n";
+    private static final String FACILITY_HEAD = "<facilities name=\"mopy\" >\n";
+    private static final String FACILITY_FOOT = "<activity type=\"rest\">\n</activity>\n</facility>\n";
+    private static final String FOOTER = "</facilities>";
+
+
+    public static void createFacilityPlan(final String inputFilepath, final String outputFilepath) {
+
+        String line;
+        String csvSplitBy = ",";
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(outputFilepath, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        writer.printf(HEADER);
+        writer.printf(FACILITY_HEAD);
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFilepath))) {
+            int id = 1;
+            while ((line = br.readLine()) != null) {
+                String[] facility = line.split(csvSplitBy);
+
+                String x, y;
+                int carLimit, busLimit, truckLimit;
+                x = facility[0].trim();
+                Double.parseDouble(x);
+                y = facility[1].trim();
+                Double.parseDouble(y);
+
+                //Wersja z limitami
+                carLimit = normalize_data(facility[2], id);
+                busLimit = normalize_data(facility[3], id);
+                truckLimit = normalize_data(facility[4], id);
+
+                writer.printf("<facility id=\"%d\" " +
+                                "x=\"%s\" " +
+                                "y=\"%s\" " +
+                                "carLimit=\"%s\" " +
+                                "busLimit=\"%s\" " +
+                                "truckLimit=\"%s\"> \n",
+                        id, x, y, carLimit, busLimit, truckLimit);
+
+
+                // Wersja bez limitów.
+//                writer.printf("<facility id=\"%d\" " +
+//                                "x=\"%s\" " +
+//                                "y=\"%s\">\n",
+//                        id, x, y);
+
+                writer.printf(FACILITY_FOOT);
+                id++;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        writer.printf(FOOTER);
+        writer.close();
+    }
+
+    private static int normalize_data(String s, int id) {
+        int ret;
+        if (s.equals("bd")) {
+            ret = 0;
+            System.out.println("Lacking data of parking places at MOP nr " + id + ".");
+            System.out.println("We assume 0 then.");
+        }
+        else {
+            try {
+                ret = Integer.parseInt(s);
+            }
+            catch (Exception e) {
+                ret = 0;
+                System.out.println("Wrong data about parking places at MOP nr " + id + ".");
+                System.out.println("We assume 0 then.");
+            }
+        }
+        return ret;
     }
 }
--- plancreator/TravelPlanCreator.java
+++ plancreator/TravelPlanCreator.java
@@ -1,38 +1,174 @@
-/* *********************************************************************** *
- * project: MOPSim
- * TravelPlanCreator.java
- * written by: mopsy-team
- * ***********************************************************************/
 package plancreator;
 
+import org.apache.commons.math3.distribution.EnumeratedDistribution;
+import org.apache.commons.math3.util.Pair;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class TravelPlanCreator {
-	
-	//Types of vehicles
-	public static final String CAR = "car";
-	public static final String TRUCK = "truck";
-	public static final String BUS = "bus";
-	
-	/* to przepisane z pythona, może trzeba lekko przeformatować string */
-	public static final String HEADER = "<?xml version=\\\"1.0\\\"?><!DOCTYPE plans SYSTEM \\\"http://www.matsim.org/files/dtd/plans_v4.dtd\\\">\\n<plans>\\n";
-	public static final String FOOTER = "</plans>\\n";
-	
-	//Path to file with town coordinates
-	public static final String TOWN_COORDINATES_PATH = "miasta.csv";
-	public static void createPlan(final String carMatrixPath, final String truckMatrixPath, final String busMatrixPath,
-			int carNr, int truckNr, int busNr, final String outputFilepath) {
-		/*
-		 * Tworzy plan podróży w oparciu o macierze podróży. Macierze podróży określają rozkład prawdopodobieństwa, z jakimi wybieramy pojazdy,
-		 * carNr, truckNr, busNr określają liczbę pojazdów danego typu (jak w pythonie, tylko tworzymy dla 3 rodzajów pojazdów). Wynik wypluwamy do outputFilepath
-		 * Jeżeli chodzi o godziny końca aktywności, to mogą być póki co losowe (bo nie mamy żadnych danych godzinowych).
-		 * Aha, jeszcze jedno - w pythonie brałem pod uwagę tylko poruszanie się pojazdów między miastami powiatowymi, bez ruchu do przejść granicznych
-		 * (i może tak zostać bo nie mamy chyba współrzędnych przejść granicznych).
-		 */
-	}
-	
-	public static void createPlan(final String carMatrixPath, final String truckMatrixPath,
-			final String busMatrixPath, final String outputFilepath) {
-		/*
-		 * To samo, tylko bierzemy tyle pojazdów, ile wynika z macierzy podróży.
-		 */
-	}
+
+    private static final int COUNTY_NUMBER = 379;
+
+    //Types of vehicles
+    private static final String CAR = "car";
+    private static final String TRUCK = "truck";
+    private static final String BUS = "bus";
+
+    private static final String HEADER = "<?xml version=\"1.0\"?>\n<!DOCTYPE plans SYSTEM \"http://www.matsim.org/files/dtd/plans_v4.dtd\">\n<plans>\n";
+    private static final String FOOTER = "</plans>\n";
+
+    //Path to file with town coordinates
+    private static final String TOWN_COORDINATES_PATH = "miasta_converted.csv";
+
+
+    private static Pair<String, String> countyTowns[];
+
+    public static void createPlan(final String carMatrixPath,
+                                  final String truckMatrixPath,
+                                  final String busMatrixPath,
+                                  int carNr, int truckNr, int busNr,
+                                  final String outputFilepath) {
+        if (carNr < 0 || truckNr < 0 || busNr < 0) {
+            System.out.println("Vehicle number cannot be negative");
+            return;
+        }
+        createPlanHelper(carMatrixPath, truckMatrixPath, busMatrixPath,
+                carNr, truckNr, busNr, outputFilepath, true);
+    }
+
+
+    public static void createPlan(final String carMatrixPath,
+                                  final String truckMatrixPath,
+                                  final String busMatrixPath,
+                                  final String outputFilepath) {
+        createPlanHelper(carMatrixPath, truckMatrixPath, busMatrixPath,
+                0, 0, 0, outputFilepath, false);
+    }
+
+    private static void createPlanHelper(final String carMatrixPath,
+                                         final String truckMatrixPath,
+                                         final String busMatrixPath,
+                                         int carNr, int truckNr, int busNr,
+                                         final String outputFilepath, boolean withNumbers) {
+        setCountyTownsCoordinates();
+
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(outputFilepath, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        writer.printf(HEADER);
+
+        addPlansFromFile(writer, CAR, carMatrixPath, carNr, withNumbers, 0);
+        addPlansFromFile(writer, TRUCK, truckMatrixPath, truckNr, withNumbers, carNr);
+        addPlansFromFile(writer, BUS, busMatrixPath, busNr, withNumbers, carNr + truckNr);
+
+        writer.printf(FOOTER);
+        writer.close();
+    }
+
+    private static void setCountyTownsCoordinates() {
+        String line;
+        String csvSplitBy = ",";
+
+        countyTowns = new Pair[COUNTY_NUMBER];
+
+        try (BufferedReader br = new BufferedReader(new FileReader(TOWN_COORDINATES_PATH))) {
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                String[] town = line.split(csvSplitBy);
+                String x, y;
+                x = town[0].trim();
+                Double.parseDouble(x);
+                y = town[1].trim();
+                Double.parseDouble(y);
+                countyTowns[i] = Pair.create(x, y);
+                i++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Double countSumAndCreateMatrix(Double travelMatrix[][], String inputFilepath) {
+        Double sum = 0.0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFilepath))) {
+            String csvSplitBy = ",";
+
+            for (int r = 0; r < COUNTY_NUMBER; r++) {
+                String line = br.readLine();
+                for (int c = 0; c < COUNTY_NUMBER; c++) {
+                    String[] row = line.split(csvSplitBy);
+
+                    travelMatrix[r][c] = Double.parseDouble(row[c]);
+                    sum += travelMatrix[r][c];
+                }
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sum;
+    }
+
+    /*
+    Writes to writer plans for vehicle of given type from csv file with inputFilepath
+    If withNumbers is false then take vehicle number from input file.
+     */
+    private static void addPlansFromFile(PrintWriter writer, String vehicleType, String inputFilepath,
+                                         int vehiclesNumber, boolean withNumbers, int idModifier) {
+
+        Double travelMatrix[][] = new Double[COUNTY_NUMBER][COUNTY_NUMBER];
+        Double sum = countSumAndCreateMatrix(travelMatrix, inputFilepath);
+
+        if (!withNumbers) {
+            vehiclesNumber = (int) Math.floor(sum);
+        }
+        if (vehiclesNumber == 0) {
+            return;
+        }
+
+        List<Pair<Pair<Integer, Integer>, Double>> probability_distribution = new ArrayList<>();
+        for (int r = 0; r < COUNTY_NUMBER; r++) {
+            for (int c = 0; c < COUNTY_NUMBER; c++) {
+                probability_distribution.add(Pair.create(Pair.create(r, c), travelMatrix[r][c]));
+            }
+        }
+        EnumeratedDistribution distribution = new EnumeratedDistribution(probability_distribution);
+        Pair<Integer, Integer>[] results = new Pair[vehiclesNumber];
+        distribution.sample(vehiclesNumber, results);
+
+
+        for (int i = 0; i < results.length; i++) {
+            int sourceTown = results[i].getFirst();
+            int targetTown = results[i].getSecond();
+            int id = i + 1 + idModifier;
+
+            Random random = new Random();
+            int endHour = 6 + random.nextInt(6);
+            int endMinute = random.nextInt(60);
+
+            writer.print(agentPlan(id, sourceTown, targetTown, vehicleType, endHour, endMinute));
+        }
+    }
+
+    private static String agentPlan(int id, int sourceTown, int targetTown,
+                                    String type, int endHour, int endMinute) {
+        String ret;
+        String x1 = countyTowns[sourceTown].getFirst();
+        String y1 = countyTowns[sourceTown].getSecond();
+        String x2 = countyTowns[targetTown].getFirst();
+        String y2 = countyTowns[targetTown].getSecond();
+        ret = String.format("<person id=\"%s\"><plan type=\"%s\"><act type=\"h\" x=\"%s\" y=\"%s\" " +
+                "end_time=\"%s:%s:00\"/><leg mode=\"%s\"/><act type=\"w\" x=\"%s\" y=\"%s\" />" +
+                "</plan></person>\n", id, type, x1, y1, endHour, endMinute, type, x2, y2);
+        return ret;
+    }
 }
