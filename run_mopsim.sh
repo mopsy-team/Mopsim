78fcd76ec81df8ec8e3ab68b3215c470e2eea7d0
NaczelnyRachmistrz <michkukula@gmail.com>
2018-04-15 22:28:22 +0200

    Typo corrected

--- run_mopsim.sh
+++ run_mopsim.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 find -name "*.java" > sources.txt
-if [ "$#" -ne 1 ]; then
-    echo "Usage: run_matsim.sh PATH/TO/MATSIM/JAR/FILE"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: run_matsim.sh PATH/TO/MATSIM/JAR/FILE PATH/TO PATH/TO/commons-math3-3.6.1.jar/FILE"
 else
     #Compiling
-    javac -cp "$1" @sources.txt
+    javac -cp "$1":"$2" @sources.txt
 
-    rm sources.txt
+   rm sources.txt
 
-    java -cp java -cp "$1":. MOPSimulator
+   java -Xmx5g -cp "$1":"$2":. MOPSimRun
 fi
